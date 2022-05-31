2e84352da6fabc228f4790221ae4613579e9ceb6
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-05-31 13:57:08 +0200

    Allow adding ladim data

--- src/ladim_aggregate/sql.py
+++ src/ladim_aggregate/sql.py
@@ -1,5 +1,8 @@
 import sqlite3
 from pathlib import Path
+import xarray as xr
+import pandas as pd
+import numpy as np
 
 
 class Particles:
@@ -7,6 +10,8 @@ class Particles:
     def __init__(self, filename=None):
         self.filename = filename
         self.connection = None
+        self.cursor = None
+        self._tables = {}
 
     def __enter__(self):
         # Return error if file exists. The backend should be a temporary file.
@@ -15,24 +20,13 @@ class Particles:
 
         self.connection = sqlite3.connect(self.filename or ':memory:')
         self.cursor = self.connection.cursor()
-        self.cursor.execute(
-            'CREATE TABLE particle_instance('
-            '  pid INTEGER'
-            '  time_idx INTEGER'
-            ')'
-        )
-        self.cursor.execute(
-            'CREATE TABLE time('
-            '  time_idx INTEGER'
-            '  time TEXT'
-            '  particle_count INTEGER'
-            ')'
-        )
-        self.cursor.execute(
-            'CREATE TABLE particle('
-            '  pid INTEGER'
-            ')'
-        )
+
+        self._add_col(tab='particle_instance', col='pid', dtype='INTEGER')
+        self._add_col(tab='particle_instance', col='time_idx', dtype='INTEGER')
+        self._add_col(tab='time', col='time_idx', dtype='INTEGER')
+        self._add_col(tab='time', col='time', dtype='TEXT')
+        self._add_col(tab='time', col='particle_count', dtype='INTEGER')
+        self._add_col(tab='particle', col='pid', dtype='INTEGER')
 
         return self
 
@@ -46,5 +40,49 @@ class Particles:
         if self.filename:
             Path(self.filename).unlink(missing_ok=True)
 
-    def add_ladim(self, particles):
-        pass
+    def _add_col(self, tab, col, dtype):
+        if tab not in self._tables:
+            self._tables[tab] = pd.DataFrame(data={col: [dtype]})
+            self.cursor.execute(f'CREATE TABLE {tab} ({col} {dtype})')
+
+        if col not in self._tables[tab].columns:
+            self._tables[tab][col] = dtype
+            self.cursor.execute(f'ALTER TABLE {tab} ADD COLUMN {col} {dtype}')
+
+    def _add_data(self, tab, cols, vals):
+        self.cursor.executemany(
+            f'INSERT INTO {tab} ({", ".join(cols)}) '
+            f'VALUES ({", ".join(["?"] * len(cols))})',
+            list(map(list, zip(*vals)))
+        )
+
+    def add_ladim(self, dset: xr.Dataset):
+        # Add columns if necessary
+        for varname in dset.variables:
+            v = dset.variables[varname]
+            dtype, data = get_sql_data_format(v.values)
+            self._add_col(tab=v.dims[0], col=varname, dtype=dtype)
+
+        # Add data
+        for tab in ['time', 'particle_instance', 'particle']:
+            cols = [c for c in self._tables[tab].columns if c in dset.variables]
+            np_vals = (dset.variables[c].values for c in cols)
+            vals = [get_sql_data_format(np_val)[1] for np_val in np_vals]
+            self._add_data(tab, cols, vals)
+
+
+def get_sql_data_format(np_data):
+    out_data = np_data
+    np_dtype = np.asarray(np_data).dtype
+    if np.issubdtype(np_dtype, np.integer):
+        sql_dtype = 'INTEGER'
+    elif np.issubdtype(np_dtype, np.float):
+        sql_dtype = 'REAL'
+    elif np.issubdtype(np_dtype, np.str):
+        sql_dtype = 'TEXT'
+    elif np.issubdtype(np_dtype, np.datetime64):
+        out_data = np_data.astype('datetime64[s]').astype(str)
+        sql_dtype = 'TEXT'
+    else:
+        raise TypeError
+    return sql_dtype, out_data
--- tests/test_sql.py
+++ tests/test_sql.py
@@ -1,7 +1,9 @@
+import pandas as pd
 from ladim_aggregate import sql
 import xarray as xr
 import pytest
 import numpy as np
+import pandas
 
 
 @pytest.fixture()
@@ -24,3 +26,36 @@ class Test_add_ladim:
     def test_can_add_ladim_dataset(self, ladim_dset):
         with sql.Particles() as p:
             p.add_ladim(ladim_dset)
+            part = pd.read_sql('SELECT * FROM particle', p.connection)
+            inst = pd.read_sql('SELECT * FROM particle_instance', p.connection)
+            time = pd.read_sql('SELECT * FROM time', p.connection)
+
+        assert len(part) == ladim_dset.dims['particle']
+        assert len(inst) == ladim_dset.dims['particle_instance']
+        assert len(time) == ladim_dset.dims['time']
+
+
+class Test_get_sql_data:
+    def test_correct_when_int(self):
+        data = np.array([1, 2, 3]).astype(int)
+        dtype, outdata = sql.get_sql_data_format(data)
+        assert outdata is data
+        assert dtype == 'INTEGER'
+
+    def test_correct_when_float(self):
+        data = np.array([1, 2, 3]).astype(float)
+        dtype, outdata = sql.get_sql_data_format(data)
+        assert outdata is data
+        assert dtype == 'REAL'
+
+    def test_correct_when_string(self):
+        data = np.array([1, 2, 3]).astype(str)
+        dtype, outdata = sql.get_sql_data_format(data)
+        assert outdata is data
+        assert dtype == 'TEXT'
+
+    def test_correct_when_date(self):
+        data = np.array(['2020-01-01', '2020-01-02']).astype('datetime64')
+        dtype, outdata = sql.get_sql_data_format(data)
+        assert outdata.tolist() == ['2020-01-01T00:00:00', '2020-01-02T00:00:00']
+        assert dtype == 'TEXT'

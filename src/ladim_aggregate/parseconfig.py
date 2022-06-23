88f017200445ef46d495967c599c69fb2c8b423e
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-06-23 09:30:56 +0200

    Add documentation

--- src/ladim_aggregate/parseconfig.py
+++ src/ladim_aggregate/parseconfig.py
@@ -1,2 +1,11 @@
 def parse_config(conf):
-    return conf
+    """
+    Parse an input configuration to ladim_aggregate, append default values and make
+    formatting changes before it is passed onwards to the program.
+
+    :param conf: Input configuration
+    :return: Output configuration, with default values appended and formatting changes
+    applied.
+    """
+    conf_out = conf.copy()
+    return conf_out
--- tests/test_parseconfig.py
+++ tests/test_parseconfig.py
@@ -1,5 +1,8 @@
-import ladim_aggregate.parseconfig
+from ladim_aggregate import parseconfig
 
 
 class Test_parse_config:
-    pass
+    def test_output_equals_input_when_unknown_keywords(self):
+        conf = dict(this_is_an_unknown_keyword="some_values")
+        conf_out = parseconfig.parse_config(conf)
+        assert conf_out == conf

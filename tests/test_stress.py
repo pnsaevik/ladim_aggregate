14b11fff4480087306fa6fa6c523435187b9b71c
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-06-24 07:39:35 +0200

    Fix input parameters

--- tests/test_stress.py
+++ tests/test_stress.py
@@ -13,10 +13,14 @@ def test_lice():
         bins=dict(time="group_by", Y=1, X=1),
         filter="(40 <= age) & (age < 170) & (Z <= 2)",
         weights="super",
-        filesplit_dims=['farm_id'],
+        filesplit_dims=['farmid', 'time'],
     )
-    config_fname = root / "aggregate.yaml"
+    config_fname = str(root / "aggregate.yaml")
     with open(config_fname, 'w', encoding='utf-8') as f:
         yaml.safe_dump(config, f)
 
     main(config_fname)
+
+
+if __name__ == "__main__":
+    test_lice()

1893335906f2a166d4e69aa56dd2f42eb03229a6
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-06-24 07:30:15 +0200

    Add stresstest module

---
+++ tests/test_stress.py
@@ -0,0 +1,22 @@
+from ladim_aggregate.script import main
+from pathlib import Path
+import yaml
+
+
+root = Path(r"C:\Users\a5606\AppData\Local\Temp\stresstest")
+
+
+def test_lice():
+    config = dict(
+        infile=r"S:\marimy\Lusearkiv_PO5\Raw2018\raw_0000.nc",
+        outfile=str(root / "count.nc"),
+        bins=dict(time="group_by", Y=1, X=1),
+        filter="(40 <= age) & (age < 170) & (Z <= 2)",
+        weights="super",
+        filesplit_dims=['farm_id'],
+    )
+    config_fname = root / "aggregate.yaml"
+    with open(config_fname, 'w', encoding='utf-8') as f:
+        yaml.safe_dump(config, f)
+
+    main(config_fname)

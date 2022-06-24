1949e0dd7980d4ac4e227d1de16ce0ae36bfdfba
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-06-24 08:51:30 +0200

    Change dimension order

--- tests/test_stress.py
+++ tests/test_stress.py
@@ -8,7 +8,7 @@ root = Path(r"S:\scratch\a5606\stresstest")
 
 def run_lice():
     config = dict(
-        infile=r"S:\marimy\Lusearkiv_PO5\Raw2018\raw_000*.nc",
+        infile=r"S:\marimy\Lusearkiv_PO5\Raw2018\raw_0010.nc",
         outfile=str(root / "count.nc"),
         bins=dict(time="group_by", Y=1, X=1),
         filter="(40 <= age) & (age < 170) & (Z <= 2)",
@@ -17,7 +17,7 @@ def run_lice():
     )
     config_fname = str(root / "aggregate.yaml")
     with open(config_fname, 'w', encoding='utf-8') as f:
-        yaml.safe_dump(config, f)
+        yaml.safe_dump(config, f, sort_keys=False)
 
     main(config_fname)
 
